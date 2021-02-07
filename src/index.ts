9c81142117b72d6acd8b431e32a7768c0164d8b9
mansi <>
2021-02-07 17:20:45 +0530

    feat : Added common function for rest call

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,18 @@ import {reqValidation} from './middleware/validation.middleware';
 import {UserKycRequest} from './dto/user-kyc-req.dto';
 import {plainToClass} from 'class-transformer';
 
+/**
+ * Function to perform customer KYC with third party system
+ * @param req 
+ * @param res 
+ */
 export const doKYC: HttpFunction = async (req, res) => {
   logger.trace('Enter :: doKYC');
   logger.debug('request data :: ', req.body);
   try {
     const userKycRequest = plainToClass(UserKycRequest, req.body);
     await reqValidation(userKycRequest);
+
     res.send('success');
   } catch (err) {
     const errDtls = errorHandler(err);
---
+++ src/util/common.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+/**
+ * Common function to make rest call
+ * @param url 
+ * @param headers 
+ * @param body 
+ */
+export async function callPostWS(url:string, headers:any, body:any){
+    try {
+        let response = await axios.post(url, body, headers)
+        if (response && response.data) {
+          return response.data
+        }
+        return response
+      } catch (error) {
+        if (error.code == 'ECONNREFUSED') {
+          throw new Error("Unable to reach the service")
+        }
+        if (error.response && error.response.data) {
+          return error.response.data
+        }
+        else return error
+      }
+}
