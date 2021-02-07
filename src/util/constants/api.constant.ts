2456330c5b7427a73fc759308fa4eb17eafd6b7c
mansi <>
2021-02-07 15:44:32 +0530

    feat : added api constants and message constants

---
+++ src/util/constants/api.constant.ts
@@ -0,0 +1,4 @@
+export const apiCons = {
+  BAD_REQUEST: 'BAD_REQUEST',
+  INTERNAL_SERVER_ERROR: 'INTERNAL_SERVER_ERROR',
+};
--- src/util/constants/msg.constant.ts
+++ src/util/constants/msg.constant.ts
@@ -1,3 +1,6 @@
 export const msgCons = {
-    MSG_VALID_LENGTH: "Cannot have more than 100 characters!!"
-}
+  MSG_VALID_LENGTH: 'Cannot have more than 100 characters!!',
+  MSG_DOC_ERROR: 'Document Error',
+  MSG_SERVER_ERROR: 'Server Error',
+  MSG_SUCCESS_KYC: 'Successfully completed kyc for customer!!',
+};
